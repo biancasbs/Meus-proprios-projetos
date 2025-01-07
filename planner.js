a036eb4b386b2e70985dfbe4e45156a626c69db8
Bianca Sousa <75484555+biancasbs@users.noreply.github.com>
2025-01-06 21:00:39 -0300

    alterações feitas com tailwindcss

--- planner.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('taskForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const taskInput = document.getElementById('taskInput');
-    const taskList = document.getElementById('taskList');
-    const newTask = document.createElement('li');
-    newTask.textContent = taskInput.value;
-    taskList.appendChild(newTask);
-    taskInput.value = '';
-});
